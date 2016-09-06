c15ce24cd4c1e3fbf7b2519e07cc4162283a8297
mike hampton <mike@clover.com>
2016-09-06 15:13:38 -0600

    SEMI-438 Remove dependency on "prototype.js"

--- public/TestSaleTipAdjust.js
+++ public/TestSaleTipAdjust.js
@@ -1,4 +1,4 @@
-require("prototype");
+var sdk = require("remote-pay-cloud-api");
 var ExampleCloverConnectorListener = require("./ExampleCloverConnectorListener.js");
 var clover = require("remote-pay-cloud");
 var TestBase = require("./TestBase.js");
@@ -9,72 +9,87 @@ var TestBase = require("./TestBase.js");
  *
  * @type {ExampleCloverConnectorListener}
  */
-var SaleTipAdjustExampleCloverConnectorListener = Class.create( ExampleCloverConnectorListener, {
-    onReady: function ($super, merchantInfo) {
-        $super(merchantInfo);
+var SaleTipAdjustExampleCloverConnectorListener = function (cloverConnector, progressinfoCallback) {
+    ExampleCloverConnectorListener.call(this, cloverConnector, progressinfoCallback);
+    this.cloverConnector = cloverConnector;
+    this.progressinfoCallback = progressinfoCallback;
+};
+
+SaleTipAdjustExampleCloverConnectorListener.prototype = Object.create(ExampleCloverConnectorListener.prototype);
+SaleTipAdjustExampleCloverConnectorListener.prototype.constructor = SaleTipAdjustExampleCloverConnectorListener;
+
+SaleTipAdjustExampleCloverConnectorListener.prototype.onReady = function (merchantInfo) {
+    ExampleCloverConnectorListener.prototype.onReady.call(this, merchantInfo);
         /*
          The connector is ready, create a sale request and send it to the device.
          */
-        var saleRequest = new clover.remotepay.SaleRequest();
+        var saleRequest = new sdk.remotepay.SaleRequest();
         saleRequest.setExternalId(clover.CloverID.getNewId());
         saleRequest.setAmount(10000);
         this.displayMessage({message: "Sending sale", request: saleRequest});
         this.cloverConnector.sale(saleRequest);
-    },
-    onSaleResponse: function (response) {
-        /*
-         The sale is complete.  It might be canceled, or successful.  This can be determined by the
-         values in the response.
-         */
-        this.displayMessage({message: "Sale response received", response: response});
-        if (!response.getIsSale()) {
-            this.displayMessage({error: "Response is not a sale!"});
-        }
-        var request = new clover.remotepay.TipAdjustAuthRequest();
+    };
 
-        request.setTipAmount(1000);
-        request.setOrderId(response.getPayment().getOrder().getId());
-        request.setPaymentId(response.getPayment().getId());
-
-        // Note, this should fail!
-        this.cloverConnector.tipAdjustAuth(request);
-    },
-
-    /**
-     * Will be called after a tip adjust request and contains the tipAmount if successful
-     * @memberof remotepay.ICloverConnectorListener
-     *
-     * @param {clover.remotepay.TipAdjustAuthResponse} response
-     * @return void
+SaleTipAdjustExampleCloverConnectorListener.prototype.onSaleResponse = function (response) {
+    /*
+     The sale is complete.  It might be canceled, or successful.  This can be determined by the
+     values in the response.
      */
-    onTipAdjustAuthResponse: function(response) {
-        this.displayMessage({message: "TipAdjustAuthResponse received", response: response});
-        if(response.getSuccess()) {
-            this.displayMessage({message: "TipAdjustAuthResponse,  !!! something is wrong, this should have failed but it succeeded !!!"});
-        }
-
-        // Always call this when your test is done, or the device may fail to connect the
-        // next time, because it is already connected.
-        this.testComplete();
-    },
-    /**
-     * Used in the test to help identify where messages come from.
-     * @returns {string}
-     */
-    getTestName: function() {
-        return "Test TipAdjustAuthResponse";
+    this.displayMessage({message: "Sale response received", response: response});
+    if (!response.getIsSale()) {
+        this.displayMessage({error: "Response is not a sale!"});
     }
-});
+    var request = new sdk.remotepay.TipAdjustAuthRequest();
+
+    request.setTipAmount(1000);
+    request.setOrderId(response.getPayment().getOrder().getId());
+    request.setPaymentId(response.getPayment().getId());
+
+    // Note, this should fail!
+    this.cloverConnector.tipAdjustAuth(request);
+};
+
+/**
+ * Will be called after a tip adjust request and contains the tipAmount if successful
+ * @memberof remotepay.ICloverConnectorListener
+ *
+ * @param response
+ * @return void
+ */
+SaleTipAdjustExampleCloverConnectorListener.prototype.onTipAdjustAuthResponse = function (response) {
+    this.displayMessage({message: "TipAdjustAuthResponse received", response: response});
+    if (response.getSuccess()) {
+        this.displayMessage({message: "TipAdjustAuthResponse,  !!! something is wrong, this should have failed but it succeeded !!!"});
+    }
+
+    // Always call this when your test is done, or the device may fail to connect the
+    // next time, because it is already connected.
+    this.testComplete();
+};
+
+/**
+ * Used in the test to help identify where messages come from.
+ * @returns {string}
+ */
+SaleTipAdjustExampleCloverConnectorListener.prototype.getTestName = function () {
+    return "Test TipAdjustAuthResponse";
+};
+
 /**
  * A very simple subclass of the tests that specifies the listener (see above)
  * that defines the test flow.
  * @type {TestBase}
  */
-TestSaleTipAdjust = Class.create( TestBase, {
-    getCloverConnectorListener: function(cloverConnector) {
-        return new SaleTipAdjustExampleCloverConnectorListener(cloverConnector, progressinfoCallback);
-    }
-});
+TestSaleTipAdjust = function (configUrl, friendlyName, progressinfoCallback) {
+    TestBase.call(this, configUrl, friendlyName, progressinfoCallback);
+};
+
+TestSaleTipAdjust.prototype = Object.create(TestBase.prototype);
+TestSaleTipAdjust.prototype.constructor = TestSaleTipAdjust;
+
+TestSaleTipAdjust.prototype.getCloverConnectorListener = function (cloverConnector) {
+    return new SaleTipAdjustExampleCloverConnectorListener(cloverConnector, progressinfoCallback);
+};
 
 /**
  * Attach the test of a sale to the testbase to facilitate calling from the main page.
--- public/TestShowMessage.js
+++ public/TestShowMessage.js
@@ -2,43 +2,54 @@ require("prototype");
 var ExampleCloverConnectorListener = require("./ExampleCloverConnectorListener.js");
 var TestBase = require("./TestBase.js");
 
-var ShowMessageExampleCloverConnectorListener = Class.create( ExampleCloverConnectorListener, {
-    onReady: function ($super, merchantInfo) {
-        $super(merchantInfo);
-        /*
-         The connector is ready, create a sale request and send it to the device.
-         */
-        this.displayMessage({message: "Sending request to read card data"});
-        this.cloverConnector.showMessage("This message was sent to this device from the test framework");
-        setTimeout(function(){
-            // Always call this when your test is done, or the device may fail to connect the
-            // next time, because it is already connected.
-            this.testComplete();
-        }.bind(this), 5000);
-    },
+var ShowMessageExampleCloverConnectorListener = function (cloverConnector, progressinfoCallback) {
+    ExampleCloverConnectorListener.call(this, cloverConnector, progressinfoCallback);
+    this.cloverConnector = cloverConnector;
+    this.progressinfoCallback = progressinfoCallback;
+};
 
-    /**
-     * Used in the test to help identify where messages come from.
-     * @returns {string}
-     */
-    getTestName: function() {
-        return "Test Displaying a message on the device";
-    }
+ShowMessageExampleCloverConnectorListener.prototype = Object.create(ExampleCloverConnectorListener.prototype);
+ShowMessageExampleCloverConnectorListener.prototype.constructor = ShowMessageExampleCloverConnectorListener;
 
-});
+ShowMessageExampleCloverConnectorListener.prototype.onReady = function (merchantInfo) {
+    ExampleCloverConnectorListener.prototype.onReady.call(this, merchantInfo);
+    /*
+     The connector is ready, you can use it to communicate with the device.
+    */
+    this.displayMessage({message: "Sending message to display"});
+    this.cloverConnector.showMessage("This message was sent to this device from the test framework");
+    setTimeout(function () {
+        // Always call this when your test is done, or the device may fail to connect the
+        // next time, because it is already connected.
+        this.testComplete();
+    }.bind(this), 5000);
+};
+
+/**
+ * Used in the test to help identify where messages come from.
+ * @returns {string}
+ */
+ShowMessageExampleCloverConnectorListener.prototype.getTestName = function () {
+    return "Test Displaying a message on the device";
+};
 
 /**
  * A very simple subclass of the tests that specifies the listener (see above)
  * that defines the test flow.
  * @type {TestBase}
  */
-TestShowMessage = Class.create( TestBase, {
-    getCloverConnectorListener: function(cloverConnector) {
-        return new ShowMessageExampleCloverConnectorListener(cloverConnector, progressinfoCallback);
-    }
-});
+TestShowMessage = function (configUrl, friendlyName, progressinfoCallback) {
+    TestBase.call(this, configUrl, friendlyName, progressinfoCallback);
+};
 
-TestBase.TestShowMessage = function(configUrl, progressinfoCallback) {
+TestShowMessage.prototype = Object.create(TestBase.prototype);
+TestShowMessage.prototype.constructor = TestShowMessage;
+
+TestShowMessage.prototype.getCloverConnectorListener = function (cloverConnector) {
+    return new ShowMessageExampleCloverConnectorListener(cloverConnector, progressinfoCallback);
+};
+
+TestBase.TestShowMessage = function (configUrl, progressinfoCallback) {
     var testObj = new TestShowMessage(configUrl, "test", progressinfoCallback);
     testObj.test();
-}
+};
