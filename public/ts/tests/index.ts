ebf8615faf9c0461784034a26a122d0e155f1a32
chris.sauerwein <chris.sauerwein@clover.com>
2017-10-05 10:45:32 -0600

    SEMI-1293 - Added unit tests using transaction options

--- public/ts/tests/index.ts
+++ public/ts/tests/index.ts
@@ -1,16 +1,16 @@
 export {TestShowMessage} from "./TestShowMessage";
 
-export {TestImagePrint} from "./TestImagePrint";
 export {TestImagePrint_1} from "./TestImagePrint_1";
 export {TestImagePrint_2} from "./TestImagePrint_2";
 export {TestImagePrint_3} from "./TestImagePrint_3";
 export {TestImagePrint_4} from "./TestImagePrint_4";
 export {TestImagePrint_5} from "./TestImagePrint_5";
+export {TestImagePrint_6} from "./TestImagePrint_6";
+export {TestImagePrint_7} from "./TestImagePrint_7";
 export {TestImagePrint_8} from "./TestImagePrint_8";
 
-export {TestDisplayOrder} from "./TestDisplayOrder";
-
 export {TestImageUrlPrint} from "./TestImageUrlPrint"
+export {TestImageUrlPrint_01} from "./TestImageUrlPrint_01"
 export {TestImageUrlPrint_1} from "./TestImageUrlPrint_1"
 export {TestImageUrlPrint_2} from "./TestImageUrlPrint_2"
 export {TestImageUrlPrint_3} from "./TestImageUrlPrint_3"
@@ -20,6 +20,31 @@ export {TestImageUrlPrint_6} from "./TestImageUrlPrint_6"
 export {TestImageUrlPrint_8} from "./TestImageUrlPrint_8"
 
 export {TestTextPrint} from "./TestTextPrint";
+
+// no point in including TestPrint because it does not have any settings associated with it.
+export {TestPrint_Image_1} from "./TestPrint_Image_1";
+export {TestPrint_Image_2} from "./TestPrint_Image_2";
+export {TestPrint_Image_3} from "./TestPrint_Image_3";
+export {TestPrint_Image_4} from "./TestPrint_Image_4";
+export {TestPrint_Image_5} from "./TestPrint_Image_5";
+export {TestPrint_Image_6} from "./TestPrint_Image_6";
+export {TestPrint_Image_7} from "./TestPrint_Image_7";
+export {TestPrint_Image_8} from "./TestPrint_Image_8";
+
+export {TestPrint_Url_1} from "./TestPrint_Url_1";
+export {TestPrint_Url_01} from "./TestPrint_Url_01";
+export {TestPrint_Url_2} from "./TestPrint_Url_2";
+export {TestPrint_Url_3} from "./TestPrint_Url_3";
+export {TestPrint_Url_4} from "./TestPrint_Url_4";
+export {TestPrint_Url_5} from "./TestPrint_Url_5";
+export {TestPrint_Url_6} from "./TestPrint_Url_6";
+export {TestPrint_Url_8} from "./TestPrint_Url_8";
+
+export {TestPrint_Text_1} from "./TestPrint_Text_1";
+
+export {TestPrint_All_1} from "./TestPrint_All_1";
+
+export {TestDisplayOrder} from "./TestDisplayOrder";
 export {TestSale} from "./TestSale";
 export {TestState} from "./TestState";
 export {TestBadConfirmFOD} from "./TestBadConfirmFOD";
@@ -66,3 +91,8 @@ export {TestSaleWithOptions_1} from "./TestSaleWithOptions_1";
 // no point in including TestAuthWithOptions because it does not have any settings associated with it.
 export {TestAuthWithOptions_1} from "./TestAuthWithOptions_1";
 
+// no point in including TestPreAuthWithOptions because it does not have any settings associated with it.
+export {TestPreAuthWithOptions_1} from "./TestPreAuthWithOptions_1";
+
+// no point in including TestManualRefundWithOptions because it does not have any settings associated with it.
+export {TestManualRefundWithOptions_1} from "./TestManualRefundWithOptions_1";
